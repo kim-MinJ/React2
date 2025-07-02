14767169c9916545fd5f256a98849f8f3f7c8eaf
kimminjae <khwhj3577@gmail.com>
2025-07-02 12:01:39 +0900

    day03 es6test

--- lesson01/src/component/App3.jsx
+++ lesson01/src/component/App3.jsx
@@ -8,6 +8,8 @@ export default function App() {
     const twice = ['나연', '모모', '다현', '지효']
     return (
         <div>
+            {/* 자식컴포넌트에 정의한 속성들(members, title, 새로운속성,,,)은
+             하나의 객체로 자식 컴포넌트에게 전달 */}
             <KpopUL members={twice} title={'트와이스'}/>
             <KpopUL members={['슈가', '지민', '제이홉', '뷔']} title={'BTS'}/>
         </div>
